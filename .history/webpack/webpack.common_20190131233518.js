40af48ee274138d3fe891c380d6d1c02359cd3af
redahas <seanredahan@gmail.com>
2019-02-03 22:45:56 +0000

    Removed untracked files from repo

--- .history/webpack/webpack.common_20190131233518.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-
-export default {
-  entry: './src/app',
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: 'bundle.js',
-  }
-};
--- .history/webpack/webpack.development_20190202072834.js
+++
@@ -1,8 +0,0 @@
-module.exports = () => ({
-  mode: 'development',
-  devServer: {
-    contentBase: path.join(__dirname, '../', 'build'),
-    compress: true,
-    port: 8000
-  }
-});
